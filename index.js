559853a6b7c6b568f232585d005810b16e57ccbf
george <grammenidis@yahoo.gr>
2020-06-05 08:56:34 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var socket = require('socket.io')
+
+// App setup
+var app = express();
+var server = app.listen(3000, () => {
+	console.log("Listening to requests on port 3000");
+})
+
+//Static files
+app.use(express.static('public'));
+
+// Socket setup
+var io = socket(server);
+io.on('connection', (socket) => {
+	console.log('moade socket connection', socket.id);
+})
